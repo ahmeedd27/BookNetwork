6d6b7ffcd5a39ed89a74992f9213c9e59c735f1d
ahmeed27 <ahmdbrl811@gmail.com>
2025-04-17 11:12:11 +0200

    updates

--- src/main/java/com/ahmed/Spring_Security/BookNetWork.java
+++ src/main/java/com/ahmed/Spring_Security/BookNetWork.java
@@ -3,16 +3,15 @@ package com.ahmed.Spring_Security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditorAware") // for the createdAt and lastModifiedAt and reference on the bean of the auditing
 @EnableAsync // used in email service
-public class MailVerFunctionalityApplication {
+public class BookNetWork {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MailVerFunctionalityApplication.class, args);
+		SpringApplication.run(BookNetWork.class, args);
 	}
 
 }
